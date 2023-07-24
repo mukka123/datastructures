53756cf6f54518bf31e67dd40e13e9d485e923a2
dosepatnool <firdosepatnool36@gmail.com>
2023-07-24 09:45:34 -0500

    commited

---
+++ CountNoOfCharacter.java
@@ -0,0 +1,14 @@
+
+public class CountNoOfCharacter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  String str="Firdose Fathima";
+  int count=0;
+  for(int i=0;i<str.length();i++)
+  {
+	  count++;
+  }
+	}
+
+}
